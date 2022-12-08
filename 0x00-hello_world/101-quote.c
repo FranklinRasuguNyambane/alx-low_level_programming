8c4bcace9aec3ec286d7ce604a441f2ee51c4086
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-08 10:22:36 -0800

    QUTT

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
-#include<stdio.h>
 #include<unistd.h>
 /**
+*
 *main - Entry point
+*
 *description:print "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+*
 *return: 1
 */
 int main(void)
