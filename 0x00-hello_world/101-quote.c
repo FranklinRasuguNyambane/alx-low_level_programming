a34f0ce3d2d4ad5a96966f7be09325bff1e9038f
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-08 08:08:36 -0800

    101_quote_c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,13 @@
 *main - prints the standard error sandwitched around the output
 *description-print "and that piece of art is useful" - Dora Korpar, 2015-10-19"
 *return:returns 1
+*
 */
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+/**
+ *Return : Returns errors(1)
+ */
+ return (1);
 }
