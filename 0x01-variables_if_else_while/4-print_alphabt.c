cbc6d13528aef8e1cde1d2751ce9fa557078bb2f
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-09 05:15:45 -0800

    LOwer case omiting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Main function of the project
+*Return: return 0 always
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
