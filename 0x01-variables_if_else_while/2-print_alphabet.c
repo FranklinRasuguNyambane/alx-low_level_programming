7ba35fd047c8fc9f3540c2432735c8a117c51bea
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-09 05:02:57 -0800

    The alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdoio.h>
+/**
+*main - Main function of the project
+*Return: return 0 always
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i<='z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
