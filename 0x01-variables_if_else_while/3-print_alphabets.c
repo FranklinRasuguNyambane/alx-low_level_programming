312d4366e11b626068285cefb41f6d1bb7fb8117
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-09 05:10:29 -0800

    ALPHAbet_combined caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Main function of the project
+*Return: return 0 always
+*/
+int main(void)
+{
+char i;
+char j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (j = 'A'; J <= 'Z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
