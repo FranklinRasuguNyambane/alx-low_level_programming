8fb659368caf263662a33b6d8800c999ef1e7f48
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-09 05:22:54 -0800

    Numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Main function of the project
+*Return: return 0 always
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
