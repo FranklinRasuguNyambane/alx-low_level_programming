f63d5c3925ac4461cc7231ab4d84fa57dc995c1a
FranklinRasuguNyambane <franklinrasugulee@gmail.com>
2022-12-09 06:21:12 -0800

    ALPHABET INV

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Main function of the project
+*Return: return 0 always
+*/
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
