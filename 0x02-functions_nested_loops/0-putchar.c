cb58c3d65adf20afdacbfddaff575f8a22d87bd0
Franklin <franklinrasugulee@gmail.com>
2022-12-13 03:04:03 -0800

    Functions and Netsed Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+*main - Main function of the project
+*Return: return 0 all the time
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
